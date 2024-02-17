1190f4b39cea1d0d65d3b871f679689c02c94572
Morino_Belgotaku <morinbruno@hotmail.be>
2024-02-17 14:39:47 +0100

    Add nav bar auto

--- app.js
+++ app.js
@@ -8,78 +8,51 @@ const port = 3000;
 
 app.set('view engine', 'ejs');
 
+// Base de données
 const database = {
   users: []
 }
+
+// Liste des liens navigable
+const nav = [
+  {
+    title: "Home",
+    url: "/"
+  },
+  {
+    title: "About",
+    url: "/about"
+  },
+  {
+    title: "Formulaire",
+    url: "/formulaire"
+  },
+  {
+    title: "Utilisateurs",
+    url: "/utilisateurs"
+  },
+  {
+    title: "EAFC Mouscron",
+    url: "/eafc"
+  }
+]
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   let prenom = req.query.prenom
   let nom = req.query.nom
-  res.render('pages/index', { prenom, nom });
-
+  res.render('pages/index', { 
+    prenom,
+    nom, 
+    nav, 
+    title: "Home",
+    description: "Page d'accueil"
+  });
 });
 
 app.get('/about', function(req, res) {
-  res.render('pages/about');
-});
-
-app.get('/hello-world', (req, res) => {
-  res.send('Hello World!')
-});
-
-app.get('/hello-world-2', (req, res) => {
-  res.render('pages/hello-world')
-});
-
-app.get('/multiplication', (req, res) => {
-  let nb1 = 10;
-  let nb2 = 5;
-  let reponse = nb1 * nb2;
-  res.render('pages/multiplication', { nb1, nb2, reponse })
-});
-
-app.get('/table-multiplication', (req, res) => {
-  let nbr = req.query.nbr ?? 1;
-  if (nbr < 1 || nbr > 10) {
-    res.send(`Erreur de nombre, il doit être compris entre 1 et 10`)
-  } else {
-    let resultat = '';
-    for (i = 1; i <= 10; i++) {
-      resultat += `${i} x ${nbr} = ${i * nbr}<br>`
-    }
-    res.send(`${resultat}`)
-  }
-});
-
-app.get('/table-multiplication-web', (req, res) => {
-  res.render('pages/table-multiplication', { req })
-});
-
-app.get('/bonjour', (req, res) => {
-  // const {nom,prenom} = req.query
-
-  let nom = req.query.nom ?? '-'
-  let prenom = req.query.prenom ?? '-'
-  res.send(`Bonjour ${nom} ${prenom}`)
-});
-
-
-app.get('/calculatrice', (req, res) => {
-  let nombre1 = parseInt(req.query.nombre1)
-  let nombre2 = parseInt(req.query.nombre2)
-  let somme = nombre1 + nombre2
-  if (nombre1 && nombre2) {
-    res.send(`${nombre1} + ${nombre2} = ${somme}`)
-  }
-  else {
-    return res.send(`Veuillez compléter ${nombre1 ? '' : 'nombre1'} ${nombre2 ? '' : 'nombre2'}`)
-  }
-});
-
-app.get('/eafc', (req, res) => {
-  res.redirect('https://www.iepsm.be/')
+  res.render('pages/about', { nav, title: "About"});
 });
 
 app.get('/supprimer', (req, res) => {
@@ -113,7 +86,9 @@ app.get('/formulaire', (req, res) => {
     nom,
     prenom,
     uuid,
-    button_modify
+    button_modify,
+    nav,
+    title: "Formulaire"
   })
 })
 
@@ -140,14 +115,73 @@ app.post('/formulaire-save', (req, res) => {
 
 app.get('/utilisateurs', (req, res) => {
   res.render('pages/utilisateurs', {
-    users: database.users
+    users: database.users,
+    nav, 
+    title: "Utilisateurs"
   })
 })
 
+app.get('/eafc', (req, res) => {
+  res.redirect('https://www.iepsm.be/')
+});
+
 app.use((req, res, next) => {
-		res.status(404).render('pages/erreurs/404')
+		res.status(404).render('pages/erreurs/404', { nav, title: "Page non trouvé" })
 
 });
+
+// app.get('/hello-world', (req, res) => {
+//   res.send('Hello World!')
+// });
+
+// app.get('/hello-world-2', (req, res) => {
+//   res.render('pages/hello-world')
+// });
+
+// app.get('/multiplication', (req, res) => {
+//   let nb1 = 10;
+//   let nb2 = 5;
+//   let reponse = nb1 * nb2;
+//   res.render('pages/multiplication', { nb1, nb2, reponse })
+// });
+
+// app.get('/table-multiplication', (req, res) => {
+//   let nbr = req.query.nbr ?? 1;
+//   if (nbr < 1 || nbr > 10) {
+//     res.send(`Erreur de nombre, il doit être compris entre 1 et 10`)
+//   } else {
+//     let resultat = '';
+//     for (i = 1; i <= 10; i++) {
+//       resultat += `${i} x ${nbr} = ${i * nbr}<br>`
+//     }
+//     res.send(`${resultat}`)
+//   }
+// });
+
+// app.get('/table-multiplication-web', (req, res) => {
+//   res.render('pages/table-multiplication', { req })
+// });
+
+// app.get('/bonjour', (req, res) => {
+//   // const {nom,prenom} = req.query
+
+//   let nom = req.query.nom ?? '-'
+//   let prenom = req.query.prenom ?? '-'
+//   res.send(`Bonjour ${nom} ${prenom}`)
+// });
+
+
+// app.get('/calculatrice', (req, res) => {
+//   let nombre1 = parseInt(req.query.nombre1)
+//   let nombre2 = parseInt(req.query.nombre2)
+//   let somme = nombre1 + nombre2
+//   if (nombre1 && nombre2) {
+//     res.send(`${nombre1} + ${nombre2} = ${somme}`)
+//   }
+//   else {
+//     return res.send(`Veuillez compléter ${nombre1 ? '' : 'nombre1'} ${nombre2 ? '' : 'nombre2'}`)
+//   }
+// });
 
 app.listen(port, () => {
   console.log('Serveur en ligne !')
